a242a7f81e2b96bfe9adf350b27d682a617d68f0
ElishaPark <elishaspark@gmail.com>
2020-05-26 19:33:54 -0700

    uploading progress for W8D2

---
+++ W8D2/rails_lite/bin/p01_basic_server.rb
@@ -0,0 +1,21 @@
+require 'rack'
+
+# Rack::Server.start(
+#   app: Proc.new do |env|
+#     ['200', {'Content-Type' => 'text/html'}, ['hello world']]
+#   end
+# )
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = "text/text"
+  res.write(req.path)
+  res.finish
+end
+
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
